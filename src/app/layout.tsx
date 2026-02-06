67368f1c4baf8d58757844a032a3be45acced03d
shashank-iter <pandeyashashank812@gmail.com>
2026-02-06 16:02:53 +0530

    fix: updated og tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { Bebas_Neue, Source_Code_Pro } from "next/font/google";
 import { AuthProvider } from "@/contexts/AuthContext";
+import OGIMage from "../../public/images/og_image.png";
 import "./globals.css";
 
 // ── Fonts ──────────────────────────────────
@@ -26,6 +27,30 @@ export const metadata: Metadata = {
   description:
     "A gamified SQL learning platform. Solve police cases by writing queries.",
   icons: { icon: "/favicon.ico" },
+  openGraph: {
+    title: "SQL Police",
+    description:
+      "A gamified SQL learning platform. Solve police cases by writing queries.",
+    url: "https://sql-police.vercel.app",
+    siteName: "SQL Police",
+    // images: [
+    //   {
+    //     // url: OGImage.src,
+    //     width: 1200,
+    //     height: 630,
+    //     alt: "SQL Police",
+    //   },
+    // ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "SQL Police",
+    description:
+      "A gamified SQL learning platform. Solve police cases by writing queries.",
+    // images: OGIMage,
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
